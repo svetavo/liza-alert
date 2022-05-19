67b1706c5a0386133a7237d65624484c2c9c121b
Anastasia Verbitskaya <nastaverb@gmail.com>
2022-05-19 19:02:27 +0300

    Добавить блок с видео

---
+++ scripts/script.js
@@ -0,0 +1,15 @@
+const sliderTime = document.querySelector('.video__range_time');
+const sliderSound = document.querySelector('.video__range_sound');
+const slide = function(slider) {
+    const min = slider.min;
+    const max = slider.max;
+    const value = slider.value;
+
+    slider.style.background = `linear-gradient(to right, #FF6600 0%, #FF6600 ${(value - min) / (max - min) * 100}%, #9397A3 ${(value - min) / (max - min) * 100}%, #9397A3 100%)`;
+
+    slider.oninput = function () {
+    this.style.background = `linear-gradient(to right, #FF6600 0%, #FF6600 ${(this.value - this.min) / (this.max - this.min) * 100}%, #9397A3 ${(this.value - this.min) / (this.max - this.min) * 100}%, #9397A3 100%)`;
+};
+};
+slide(sliderTime);
+slide(sliderSound);
