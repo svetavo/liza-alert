545e7647ca1a66c5310b1da3404aff933cb1d760
z216ero <moskinoEU@yandex.ru>
2022-05-25 20:13:07 +0300

    MicroFix

--- scripts/script.js
+++ scripts/script.js
@@ -89,15 +89,6 @@ sliderTime.addEventListener('change', setProgress)
 video.addEventListener('timeupdate', updateTimeElapsed);
 
 
-const slide = function(slider) {
-    const min = slider.min;
-    const max = slider.max;
-    const value = slider.value;
-    slider.style.background = `linear-gradient(to right, #FF6600 0%, #FF6600 ${(value - min) / (max - min) * 100}%, #9397A3 ${(value - min) / (max - min) * 100}%, #9397A3 100%)`;
-    slider.oninput = function () {
-    this.style.background = `linear-gradient(to right, #FF6600 0%, #FF6600 ${(this.value - this.min) / (this.max - this.min) * 100}%, #9397A3 ${(this.value - this.min) / (this.max - this.min) * 100}%, #9397A3 100%)`;
-};
-};
 
 slide(sliderSound);
 
